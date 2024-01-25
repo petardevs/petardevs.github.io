539ad096a09f0862a123e45d71bf1cedbfa8b251
petarpla <124912810+petardevs@users.noreply.github.com>
2024-01-25 23:40:25 +0100

    26.1

--- assets/js/skripta.js
+++ assets/js/skripta.js
@@ -1,30 +1,57 @@
-/* var tabs = document.querySelectorAll(".w-tab-link");
-var backBtn = document.querySelector(".back-icon");
-var tabContent = document.querySelector(".w-tab-content");
+/* */
 
 
-tabContent.style.display = "none";
 
-tabs.forEach(tab => {
-   tab.addEventListener("click", ()=> {
-      let current = tab;
 
-      tabs.forEach(e => {
-         e.style.display = "none";
+
+
+
+function myFunction(x) {
+   if (x.matches) {
+      var tabs = document.querySelectorAll(".w-tab-link");
+      var backBtn = document.querySelector(".back-icon");
+      var tabContent = document.querySelector(".w-tab-content");
+
+
+      tabContent.style.display = "none";
+
+      tabs.forEach(tab => {
+         tab.addEventListener("click", () => {
+            let current = tab;
+
+            tabs.forEach(e => {
+               e.style.display = "none";
+            });
+
+            current.style.display = "block";
+            backBtn.style.display = "block";
+            tabContent.style.display = "block";
+         });
       });
 
-      current.style.display = "block";
-      backBtn.style.display = "block";
+
+      backBtn.addEventListener("click", () => {
+         tabs.forEach(tab => {
+            tab.style.display = "inline-block";
+
+         });
+         backBtn.style.display = "none";
+         tabContent.style.display = "none";
+      });
+   }
+   else {
+      tabs.forEach(tab => {
+         tab.style.display = "inline-block";
+
+      });
+      backBtn.style.display = "none";
       tabContent.style.display = "block";
-   });
+   }
+}
+var x = window.matchMedia("(max-width: 767px)");
+
+window.addEventListener("resize", () => {
+   myFunction(x);
 });
 
-
-backBtn.addEventListener("click", ()=> {
-   tabs.forEach(tab => {
-      tab.style.display = "block";
-
-   });
-   backBtn.style.display = "none";
-   tabContent.style.display = "none";
-}); */
+myFunction(x);
