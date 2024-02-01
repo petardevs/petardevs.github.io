e931f3239e9a13268d62064ff91e251e134c42d2
petarpla <124912810+petardevs@users.noreply.github.com>
2024-02-01 12:20:12 +0100

    01.02.24 12:20

--- assets/js/skripta.js
+++ assets/js/skripta.js
@@ -1,39 +1,28 @@
-/* */
+const tabs = document.querySelectorAll(".w-tab-link");
+const backBtn = document.querySelector(".back-icon");
+const tabContent = document.querySelector(".w-tab-content");
 
+tabContent.style.display = "none";
 
+tabs.forEach(tab => {
+   tab.addEventListener("click", () => {
+      let current = tab;
 
-
-
-
-
-      var tabs = document.querySelectorAll(".w-tab-link");
-      var backBtn = document.querySelector(".back-icon");
-      var tabContent = document.querySelector(".w-tab-content");
-
-
-      tabContent.style.display = "none";
-
-      tabs.forEach(tab => {
-         tab.addEventListener("click", () => {
-            let current = tab;
-
-            tabs.forEach(e => {
-               e.style.display = "none";
-            });
-
-            current.style.display = "block";
-            backBtn.style.display = "block";
-            tabContent.style.display = "block";
-         });
+      tabs.forEach(e => {
+         e.style.display = "none";
       });
 
+      current.style.display = "block";
+      backBtn.style.display = "block";
+      tabContent.style.display = "block";
+   });
+});
 
-      backBtn.addEventListener("click", () => {
-         tabs.forEach(tab => {
-            tab.style.display = "inline-block";
 
-         });
-         backBtn.style.display = "none";
-         tabContent.style.display = "none";
-      });
-   
+backBtn.addEventListener("click", () => {
+   tabs.forEach(tab => {
+      tab.style.display = "inline-block";
+   });
+   backBtn.style.display = "none";
+   tabContent.style.display = "none";
+});
